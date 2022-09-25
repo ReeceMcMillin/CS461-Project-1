caa0eceb957ace051189e2cc72f2eaeec5602328
Reece McMillin <reecemcmillin@gmail.com>
2022-09-25 12:14:37 -0500

    Added comments, README, and a screenshot of example usage. Probably final submission.

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 This is a simple implementation of A* for my Intro to AI course.
 
+The `Adjacencies-custom.txt` file has a couple changes to make more intuitive paths. Turns out Viola is kind of a choke point for a lot of towns in that area. It doesn't matter for the assignment, but it helped me gut check that unintuitive paths weren't an actual error in my script.
+
 # Usage
 
-```bash
-[23:08] pop-os:program1 (main %) | python3 main.py
-Start: Rago
-Goal: Coldwater
-Rago -> Sawyer -> Pratt -> Greensburg -> Coldwater
-```
+![A screenshot of the script in action showing several examples of start/goal cities along with their shortest path outputs.](example.png)
--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
 from math import sqrt
-from typing import List, Set, Tuple
+from typing import DefaultDict, Dict, List, Set, Tuple
 import heapq
 
 
@@ -9,24 +9,24 @@ def manhattan_distance(src: Tuple, dest: Tuple):
     return sqrt(abs(src[0] - dest[0]) + abs(src[1] - dest[1]))
 
 
-def backtrack(came_from: dict, city: str):
-    path = [city]
+def backtrack(came_from: Dict[str, str], city: str):
+    path: List[str] = [city]
     while came_from.get(city) is not None:
         city = came_from.get(city)
         path.append(city)
     return list(reversed(path))
 
 
-@dataclass
 class Node:
-    name: str
-    location: Tuple[float, float]
+    def __init__(self, name: str, location: Tuple[float, float]):
+        self.name = name
+        self.location = location
 
-    def __eq__(self, other: "Node") -> bool:
-        return self.name == other.name
+        def __eq__(self, other: Node) -> bool:
+            return self.name == other.name
 
-    def __hash__(self) -> int:
-        return hash(self.name)
+        def __hash__(self) -> int:
+            return hash(self.name)
 
 
 @dataclass
@@ -56,46 +56,60 @@ class Graph:
         node_names = list(map(lambda x: x.name, self.nodes))
         if name in node_names:
             return self.nodes[node_names.index(name)]
+        else:
+            raise KeyError("Node not found.")
 
     def a_star(self, start: str, goal: str):
-        """Mostly translated from Wikipedia's pseudocode for A*
+        """I used Wikipedia's article on A* as a major resource for this implementation!
         https://en.wikipedia.org/wiki/A*_search_algorithm"""
 
         start_node: Node = self.get_node(start)
         goal_node: Node = self.get_node(goal)
 
-        came_from = dict()
+        # For each city we visit, we'll store the city just prior in `came_from` as a {city: prior} k/v pair.
+        came_from: Dict[str, str] = dict()
 
-        g_score = defaultdict(lambda: float("inf"))
-        f_score = defaultdict(lambda: float("inf"))
+        # Consider the distances to be infinite until we can show otherwise.
+        g_score: DefaultDict[str, float] = defaultdict(lambda: float("inf"))
+        f_score: DefaultDict[str, float] = defaultdict(lambda: float("inf"))
 
         heuristic = lambda node: manhattan_distance(node.location, goal_node.location)
 
         g_score[start] = 0
         f_score[start] = heuristic(start_node)
 
+        # open_set is the foundation for a min-heap ordered by f_score.
         open_set = [(f_score[start], start)]
 
+        # Continue looping until we can't pop a city off the open set anymore.
         while current := heapq.heappop(open_set):
             current_node = self.get_node(current[1])
 
+            # If we've reached the goal, print out the path we took to get here and exit.
             if current_node.name == goal:
                 print(" -> ".join(backtrack(came_from, current_node.name)))
                 return
 
             for neighbor in self.neighbors(current_node.name):
+                # For each neighbor, assign a tentative g_score as the current node's known
+                # g_score plus the Manhattan distance from the node to our goal location.
                 tentative_g_score = g_score[current_node.name] + manhattan_distance(
                     current_node.location, neighbor.location
                 )
 
+                # Since the default g_score is infinity, any previously unvisited neighbors will be visited here.
                 if tentative_g_score < g_score[neighbor.name]:
                     came_from[neighbor.name] = current_node.name
                     g_score[neighbor.name] = tentative_g_score
                     f_score[neighbor.name] = tentative_g_score + heuristic(neighbor)
 
+                    # If the current neighbor isn't in the open_set, go ahead and heap-push it.
                     if neighbor.name not in map(lambda x: x[1], open_set):
                         heapq.heappush(
                             open_set, (f_score[neighbor.name], neighbor.name)
                         )
+
+        # Since we return early for successful traversals, we can
+        # be sure a path doesn't exist once we've exited the loop.
         print("No paths found.")
         return False
